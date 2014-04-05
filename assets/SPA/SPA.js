29c1f37a65e599160e98d92bde713de18443e845
JAlvarado <jose.alvarado@move.com>
2014-04-05 12:51:57 -0700

    refactored to use aloha directive

--- assets/SPA/SPA.js
+++ assets/SPA/SPA.js
@@ -1,3 +1,11 @@
+/*
+This angular.js app is using angular ui.router module.  You can find documentation here:
+
+ https://github.com/angular-ui/ui-router
+ https://egghead.io/lessons/angularjs-introduction-ui-router
+ https://github.com/angular-ui/ui-router/wiki/URL-Routing
+ */
+
 var eazyNotes = angular.module('EazyNotes', ['ui.router'])
     .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
         $stateProvider
@@ -20,38 +28,47 @@ var eazyNotes = angular.module('EazyNotes', ['ui.router'])
         $urlRouterProvider.otherwise('/');
     }]);
 
-
-
-//https://github.com/angular-ui/ui-router
-//https://egghead.io/lessons/angularjs-introduction-ui-router
-//https://github.com/angular-ui/ui-router/wiki/URL-Routing
-
 eazyNotes.factory('EasyNotesService', function ($http) {
-    var EasyNotesService = function(data) {
+    var EasyNotesService = function (data) {
         angular.extend(this, data);
     };
 
-    EasyNotesService.getProjects = function(successCallback) {
+    EasyNotesService.getProjects = function (successCallback) {
         return $http.get('api/projects')
-            .then(function(response) {
+            .then(function (response) {
                 successCallback(response.data);
                 return new EasyNotesService(response.data);
             });
     };
 
-    EasyNotesService.getVersions = function(project, successCallback) {
+    EasyNotesService.getVersions = function (project, successCallback) {
         return $http.get('api/' + project + '/versions')
-            .then(function(response) {
+            .then(function (response) {
                 successCallback(response.data);
                 return new EasyNotesService(response.data);
             });
     };
 
-    EasyNotesService.getNotes = function(project, version, successCallback) {
+    EasyNotesService.getNotes = function (project, version, successCallback) {
         return $http.get('api/' + project + '/' + version)
-            .then(function(response) {
+            .then(function (response) {
                 successCallback(response.data);
                 return new EasyNotesService(response.data);
+            });
+    };
+
+    EasyNotesService.update = function (data, successCallback) {
+        var url = "api/" + data.project + "/" + data.version + "/update";
+        var data = {
+            content: data.content,
+            contentId: data.contentId,
+            pageId: data.pageId,
+            project: data.project,
+            version: data.version
+        };
+        $http.post(url, data)
+            .success(function (data, status, headers, config) {
+                successCallback(data, status, headers, config);
             });
     };
 
@@ -61,11 +78,10 @@ eazyNotes.factory('EasyNotesService', function ($http) {
 eazyNotes.controller("ProjectsController", function ($scope, EasyNotesService, $location) {
     console.log("inside projects controller!");
     $scope.projects = [];
-    EasyNotesService.getProjects(function(data)
-    {
+    EasyNotesService.getProjects(function (data) {
         console.log("retrieving projects...");
         //console.log(data);
-        $scope.projects =  data.projects;
+        $scope.projects = data.projects;
     });
 });
 
@@ -73,11 +89,10 @@ eazyNotes.controller("VersionsController", function ($scope, EasyNotesService, $
     console.log("inside versions controller!");
     $scope.project = $stateParams.project;
     $scope.versions = [];
-    EasyNotesService.getVersions($scope.project, function(data)
-    {
+    EasyNotesService.getVersions($scope.project, function (data) {
         console.log("retrieving versions...");
         //console.log(data.versions);
-        $scope.versions =  data.versions;
+        $scope.versions = data.versions;
     });
 });
 
@@ -86,62 +101,50 @@ eazyNotes.controller("NotesController", function ($scope, EasyNotesService, $sta
     $scope.project = $stateParams.project;
     $scope.version = $stateParams.version;
     $scope.versions = [];
-    EasyNotesService.getNotes($scope.project, $scope.version, function(data)
-    {
+    EasyNotesService.getNotes($scope.project, $scope.version, function (data) {
         console.log("retrieving notes...");
         //console.log(data.issues);
-        $scope.issues =  data.issues;
+        $scope.issues = data.issues;
         $scope.content = $sce.trustAsHtml(data.content);
     });
+
+    $scope.update = function (notes) {
+        EasyNotesService.update(notes, function (data, status, headers, config) {
+            console.log("notes updated successfully.");
+        });
+    };
 });
 
-eazyNotes.directive("editable", function() {
+eazyNotes.directive("editable", function () {
     var config = {
         replace: true,
         transclude: false,
         restrict: 'A',
-        link: function(scope, element, attrs) {
-            Aloha.ready( function() {
+        link: function (scope, element, attrs) {
+            Aloha.ready(function () {
                 var $ = Aloha.jQuery;
                 $(element).aloha();
-
-                Aloha.require( ['aloha', 'aloha/jquery'], function( Aloha, jQuery) {
-
-                    // save all changes after leaving an editable
-                    Aloha.bind('aloha-editable-deactivated', function(){
+                Aloha.require(['aloha', 'aloha/jquery'], function (Aloha, jQuery) {
+                    Aloha.bind('aloha-editable-deactivated', function () {
+                        var $ = jQuery;
                         var content = Aloha.activeEditable.getContents();
                         var contentId = Aloha.activeEditable.obj[0].id;
                         var pageId = window.location.pathname;
-                        var project = jQuery("#project").val();
-                        var version = jQuery("#version").val();
+                        var project = $("#project").val();
+                        var version = $("#version").val();
 
-                        // textarea handling -- html id is "xy" and will be "xy-aloha" for the aloha editable
-                        if ( contentId.match(/-aloha$/gi) ) {
-                            contentId = contentId.replace( /-aloha/gi, '' );
+                        // textarea handling -- html id is "xy" and
+                        // will be "xy-aloha" for the aloha editable
+                        if (contentId.match(/-aloha$/gi)) {
+                            contentId = contentId.replace(/-aloha/gi, '');
                         }
-                        console.log("saving aloha content");
-                        console.log(content);
-                        console.log(project);
-                        console.log(version);
-                        var request = jQuery.ajax({
-                            url: "/api/" + project + "/" + version + "/update",
-                            type: "POST",
-                            data: {
-                                content : content,
-                                contentId : contentId,
-                                pageId : pageId,
-                                project: project,
-                                version: version
-                            },
-                            dataType: "html"
-                        });
 
-                        request.done(function(msg) {
-                            jQuery("#log").html( msg ).show().delay(800).fadeOut();
-                        });
-
-                        request.error(function(jqXHR, textStatus) {
-                            alert( "Request failed: " + textStatus );
+                        scope.update({
+                            content: content,
+                            contentId: contentId,
+                            pageId: pageId,
+                            project: project,
+                            version: version
                         });
                     });
                 });
