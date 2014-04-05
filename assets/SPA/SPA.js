f35a87dbe85e742c25d8d2fa891c9ff093e67d86
JAlvarado <jose.alvarado@move.com>
2014-04-05 12:10:22 -0700

    got aloha working

--- assets/SPA/SPA.js
+++ assets/SPA/SPA.js
@@ -20,6 +20,8 @@ var eazyNotes = angular.module('EazyNotes', ['ui.router'])
         $urlRouterProvider.otherwise('/');
     }]);
 
+
+
 //https://github.com/angular-ui/ui-router
 //https://egghead.io/lessons/angularjs-introduction-ui-router
 //https://github.com/angular-ui/ui-router/wiki/URL-Routing
@@ -91,4 +93,61 @@ eazyNotes.controller("NotesController", function ($scope, EasyNotesService, $sta
         $scope.issues =  data.issues;
         $scope.content = $sce.trustAsHtml(data.content);
     });
+});
+
+eazyNotes.directive("editable", function() {
+    var config = {
+        replace: true,
+        transclude: false,
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            Aloha.ready( function() {
+                var $ = Aloha.jQuery;
+                $(element).aloha();
+
+                Aloha.require( ['aloha', 'aloha/jquery'], function( Aloha, jQuery) {
+
+                    // save all changes after leaving an editable
+                    Aloha.bind('aloha-editable-deactivated', function(){
+                        var content = Aloha.activeEditable.getContents();
+                        var contentId = Aloha.activeEditable.obj[0].id;
+                        var pageId = window.location.pathname;
+                        var project = jQuery("#project").val();
+                        var version = jQuery("#version").val();
+
+                        // textarea handling -- html id is "xy" and will be "xy-aloha" for the aloha editable
+                        if ( contentId.match(/-aloha$/gi) ) {
+                            contentId = contentId.replace( /-aloha/gi, '' );
+                        }
+                        console.log("saving aloha content");
+                        console.log(content);
+                        console.log(project);
+                        console.log(version);
+                        var request = jQuery.ajax({
+                            url: "/api/" + project + "/" + version + "/update",
+                            type: "POST",
+                            data: {
+                                content : content,
+                                contentId : contentId,
+                                pageId : pageId,
+                                project: project,
+                                version: version
+                            },
+                            dataType: "html"
+                        });
+
+                        request.done(function(msg) {
+                            jQuery("#log").html( msg ).show().delay(800).fadeOut();
+                        });
+
+                        request.error(function(jqXHR, textStatus) {
+                            alert( "Request failed: " + textStatus );
+                        });
+                    });
+                });
+            });
+        }
+    };
+
+    return config;
 });
